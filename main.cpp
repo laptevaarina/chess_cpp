b2aecc05ac7676dd120c1ce01b872210fb377357
laptevaarina <arina.lapteva2016@yandex.ru>
2020-11-08 21:41:45 +0300

    board

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <Window.h>
+#include <Graph.h>
+#include <GUI.h>
+#include <Simple_window.h>
+using namespace Graph_lib;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    Simple_window win {Point{400, 600}, 400, 600, "test"};
+    Graph_lib::Rectangle x {Point{200, 300}, 100, 100} ;
+    win.attach(x);
+    win.wait_for_button();
 }
